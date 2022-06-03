c6fd630a20e282f752631d189fe9af6c05e6dc68
Stepix <darklightgamesz@gmail.com>
2022-06-03 21:25:14 +0500

    ...

---
+++ Compiler/ApatiteCompiler/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using Utils;
+
+namespace ApatiteCompiler
+{
+    internal class Program
+    {
+
+        const string CompilerVersion = "0.1.0";
+
+        const string DefaultProgram = "import std \n\nfunction main() { \n    print(\"Hello, world\") \n}";
+
+        static List<string> added_libraries = new List<string>();
+
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.WriteLine(@"Apatite Compiler "+ CompilerVersion);
+                Console.WriteLine("Available options:");
+                Console.WriteLine("     build <full : optional> -> Compile your program into cpp code and into executable if 'full' option was used");
+                Console.WriteLine("     new [name : optional] -> Create new project");
+
+
+                return;
+            }
+
+            if(args[0] == "new")
+            {
+                string app_name = "app";
+                if(args.Length > 1)
+                    app_name = args[1];
+
+
+                Directory.CreateDirectory(app_name);
+                Directory.CreateDirectory(app_name+"\\src");
+                Directory.CreateDirectory(app_name + "\\src\\cpp");
+                Directory.CreateDirectory(app_name+"\\bin");
+
+
+
+                File.Create(app_name + "\\" + "src\\main.aptt").Close();
+                File.WriteAllText(app_name + "\\" + "src\\main.aptt", DefaultProgram);
+
+
+            }
+
+            else if(args.Length > 1)
+            {
+                if(args[0] == "build")
+                {
+                    string[] result = compile(Directory.GetCurrentDirectory()+"\\src\\main.aptt",true,"aptt");
+
+                    if(result[0] == "0")
+                    {
+                        if (!File.Exists(Directory.GetCurrentDirectory() + "\\src\\cpp\\source.cpp"))
+                            File.Create(Directory.GetCurrentDirectory() + "\\src\\cpp\\source.cpp").Close();
+
+      
+                        File.WriteAllText(Directory.GetCurrentDirectory() + "\\src\\cpp\\source.cpp", result[1]);
+
+                        Console.WriteLine("Successfuly compiled main");
+
+                        if (args.Length > 1)
+                        {
+                            if(args [1] == "full")
+                            {
+                                Console.WriteLine("\nCompiling C++ Source");
+                                Process.Start("cmd", "/C"+ AppDomain.CurrentDomain.BaseDirectory+"mingw64\\bin\\g++ " + Directory.GetCurrentDirectory() + "\\src\\cpp\\source.cpp -o " + Directory.GetCurrentDirectory() + "\\bin\\app");
+                            }
+                        }
+
+                        Console.WriteLine("\nTask finished.");
+
+
+
+                        
+                    }
+                    else if(result[0] == "1")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Cannot find file: "+ Directory.GetCurrentDirectory() + "\\src\\main.aptt");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        return;
+                    }
+
+
+
+                }
+            }
+        }
+
+        public static string[] compile(string path, bool include_main, string language)
+        {
+            string c_source = "";
+
+            if (!File.Exists(path))
+                return new string[2] {"1",c_source};
+
+            string aptt_source = File.ReadAllText(path);
+
+            if(language == "cpp")
+            {
+                return new string[2] {"0", aptt_source};
+            }
+                
+
+            if(include_main)
+            {
+                c_source += "int exit_code = 0;\n";
+            }
+
+            foreach (string s in aptt_source.Split('\n'))
+            {
+                string line = s.Trim();
+
+                if (line.line().is_import()) // import std     -> "lib\std.aptt"
+                {
+                    if (!added_libraries.Contains(line.line().get_import_path()))
+                    {
+                        string[] res = compile(line.line().get_import_path(), false, line.line().get_import_ext());
+                        if(res[0] == "0")
+                        {
+                            Console.WriteLine("Successfuly compiled library: " + line.line().get_import_name());    
+                            c_source += res[1] + "\n";
+                            added_libraries.Add(line.line().get_import_path());
+                        }
+                        else
+                        {
+                            Console.WriteLine(line.line().get_import_path());
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Cannot compile: " + line.line().get_import_path());
+                            Console.ForegroundColor = ConsoleColor.Gray;
+
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Skipping library: " + line.line().get_import_name() + " (already included)");
+                    }
+
+                }
+                else if(line.line().is_function_defenition())
+                {
+                    //                      void                                        __FUNCTION__APATITE__function                                                               (int arg1, int arg2)
+                    c_source += line.line().get_function_defenition_return_type() +" __FUNCTION__APATITE__"+line.line().get_function_defenition_name()+"("+line.line().get_function_defenition_args().line().remmap_f_d_type_system()+")";
+
+                    if (line.EndsWith("{"))
+                        c_source += "{";
+
+                    c_source += "\n";
+                }
+                else if(line.line().is_function_call())
+                {
+                    c_source += line.line().remapp_apatite_functions() + ";\n";
+                }   
+                else if(line.line().should_keep())
+                {
+                    c_source += line + "\n";
+                }
+                else if(line.line().is_var_def())
+                {
+                    c_source += line.line().aptt_to_cpp_var() + ";\n";
+                }
+                else if(line.line().is_var_set())
+                {
+                    c_source += line.Split('=')[0]+" = "+ line.Split('=')[1].line().remapp_apatite_functions() + ";\n";
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Cannot compile "+path);
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Unexpected line: " + line);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    return new string[2] {"-1",c_source};
+                }
+
+
+            }
+
+            if (include_main)
+            {
+                c_source += "int main(void) { __FUNCTION__APATITE__main(); return exit_code;}";
+            }
+
+
+            return new string[2] {"0", c_source};
+        }
+    }
+}
---
+++ Compiler/Utils/Utils.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.IO;
+
+namespace Utils
+{
+
+    public enum importType
+    {
+        normal_aptt = 0,
+        normal_c = 1,
+        normal_folder = 2,
+        user_aptt = 3,
+        user_c = 4,
+        user_folder = 5,
+        unknown = 6
+           
+    }
+
+    public class line
+    {
+        string val;
+
+        public line(string v)
+        {
+            this.val = v;
+        }
+
+
+        public bool is_import()
+        {
+            return val.StartsWith("import ");
+        }
+        public importType get_import_type()
+        {
+            string line = val.remove_start("import ");
+
+            if (line.EndsWith(".aptt")) // import lib.aptt
+                return importType.normal_aptt;
+            if (line.EndsWith(".cpp"))// import lib.cpp
+                return importType.normal_c;
+            if (line.EndsWith("*")) // import folder*
+                return importType.normal_folder;
+
+            if (line.StartsWith("\"") && line.EndsWith("\""))
+            {
+                line = line.brackets("\"");
+
+                if (line.EndsWith(".aptt")) // import "lib.aptt"
+                    return importType.user_aptt;
+                if (line.EndsWith(".cpp")) // import "lib.cpp"
+                    return importType.user_c;
+                if (line.EndsWith("*")) // import "folder*"
+                    return importType.user_folder;
+
+            }
+
+            return importType.normal_aptt;
+
+        }
+        public string get_import_ext()
+        {
+            string nval = val.remove_start("import ");
+            if (nval.Split(".").Length > 1) //  lib.aptt or  "lib.aptt"
+            {
+                return nval.Split(".")[nval.Split(".").Length - 1].remove_end("\"");
+            }
+
+            if (nval.brackets("\"") == nval) // std
+            {
+                string dir = AppDomain.CurrentDomain.BaseDirectory;
+                if (File.Exists(dir + "lib\\" + nval + ".aptt"))
+                    return "aptt";
+                if (File.Exists(dir + "lib\\" + nval + ".cpp"))
+                    return "cpp";
+
+            }
+            else
+            {
+                if (File.Exists(nval + ".aptt"))
+                    return "aptt";
+                if (File.Exists(nval + ".cpp"))
+                    return "cpp";
+            }
+
+            return "aptt";
+
+        }
+        public string get_import_name() // std std.aptt "std.aptt" "not\simple.at.all.aptt"
+        {
+            string nval = val.remove_start("import ");
+            if (val.Split("\\").Length > 0)
+                nval = nval.Split("\\")[nval.Split("\\").Length - 1];
+
+            if (val.Split(".").Length > 0)
+                nval = nval.brackets("\"").remove_end("." + nval.Split(".")[nval.Split(".").Length - 1]);
+
+            return nval;
+        }
+        public string get_import_path() // import std -> "lib\std.aptt"        import "std" -> "std"
+        {
+            if (get_import_type() == importType.normal_c || get_import_type() == importType.normal_aptt || get_import_type() == importType.normal_folder) // -> not user defined
+            {
+                return AppDomain.CurrentDomain.BaseDirectory+"lib\\" + get_import_name() + "." + get_import_ext();
+            }
+            else
+            {
+                return val.remove_start("import ").brackets("\"");
+            }
+
+        }
+
+        public bool is_function_defenition() // function name() -> int {
+        {
+            return val.StartsWith("function ");
+        }
+
+        public string get_function_defenition_name()
+        {
+            return val.remove_start("function ").Split("(")[0];
+        }
+        public string get_function_defenition_return_type() // function name() -> TYPE {
+        {
+            if(val.splitstr("->").Length > 1)
+                return val.splitstr("->")[1].TrimEnd('{').Trim();
+
+            return "void";
+        }
+        public string get_function_defenition_args() // function name(a,b) -> type           a,b
+        {
+            return val.Trim().TrimEnd('{').remove_start("function ").remove_start(get_function_defenition_name()).Trim().remove_end(get_function_defenition_return_type()).Trim().remove_end("->").Trim().brackets("(", ")");
+        }
+        public string remmap_f_d_type_system() // a: int, b: float      int a, int b
+        {
+            string res = "";
+
+            if(val == "")
+                return res;
+
+            foreach(string c in val.Split(","))
+            {
+                res += c.Trim().Split(":")[1].Trim()+" "+ c.Trim().Split(":")[0].Trim() + ", ";
+            }
+            res = res.Trim().TrimEnd(',');
+
+            return res;
+        }
+
+        public bool is_function_call() // call()
+        {
+
+            if(!val.Contains("(")) // call)
+                return false;
+            if(!val.Contains(")")) // call(
+                return false;
+            if (val.Split("(")[0].Contains(" ")) //c all()
+                return false;
+            if (val.Split("(")[0].Trim().Length == 0) // ()
+                return false;
+
+            return true;
+        }
+
+        public string get_function_c_name()
+        {
+            return val.Trim().Split('(')[0];
+        }
+        public string get_function_c_args()
+        {
+            return val.Trim().remove_start(get_function_c_name()).brackets("(", ")");
+        }
+
+        public string space_nonstr()
+        {
+            string spaced = "";
+
+
+            bool in_str = false;
+            char pc = ' ';
+            foreach(char c in val)
+            {
+                if (c == '"' && pc != '\\')
+                    in_str = !in_str;
+
+                if (c == '+' && !in_str)
+                {
+                    spaced += " + ";
+                }
+
+                if (c == '-' && !in_str)
+                {
+                    spaced += " - ";
+                }
+
+                if (c == '*' && !in_str)
+                {
+                    spaced += " * ";
+                }
+
+                if (c == '/' && !in_str)
+                {
+                    spaced += " / ";
+                }
+
+                if (c == '%' && !in_str)
+                {
+                    spaced += " % ";
+                }
+
+                if (c == '!' && !in_str)
+                {
+                    spaced += " ! ";
+                }
+                else
+                {
+                    spaced += c;
+                }
+                    
+
+                pc = c;
+            }
+
+            return spaced;
+        }
+
+        public bool is_var_def() // var a: int = 5
+        {
+            if(!val.StartsWith("var"))
+                return false;
+            if (!(val.Split(" ").Length >= 5))
+                return false;
+            if(!val.Split(" ")[1].EndsWith(":"))
+                return false;
+
+            return true;
+        }
+
+        public bool is_var_set()
+        {
+            return val.Split("=").Length == 2;
+        }
+
+        public string aptt_to_cpp_var()
+        {
+            return val.Split(" ")[2] + " " + val.Split(" ")[1].TrimEnd(':') + " = "+ val.Split("=")[1].line().remapp_apatite_functions() ;
+        }
+
+        public string remapp_apatite_functions()
+        {
+            string result = "";
+            string nval = space_nonstr().Replace("(","( ").Replace(")"," )");
+
+
+            foreach (string i in nval.Split(' '))
+            {
+
+                if((i.line().is_function_call() || (i+")").line().is_function_call()))
+                {
+                    result += "__FUNCTION__APATITE__" + i + " ";
+                }
+                else
+                {
+                    result += i + " ";
+                }
+            }
+
+            return result;
+                    
+        }
+
+        public bool should_keep()
+        {
+
+            if(val.Trim() == "" || val.Trim() == string.Empty || val == "{" || val == "}")
+            {
+                return true;
+            }
+
+
+            return false;
+        }
+    }
+
+    public static class StringExtension
+    {
+        public static string[] splitstr(this string str, string by)
+        {
+             return str.Split(new string[] { by }, StringSplitOptions.None);
+        }
+        public static string[] splitstr_trim(this string str, string by)
+        {
+            return str.Split(new string[] { by }, StringSplitOptions.TrimEntries);
+        }
+        public static line line(this string str)
+        {
+            return new line(str);
+        }
+        public static string remove_start(this string str, string expected)
+        {
+            if (str.StartsWith(expected))
+            {
+                return str.Remove(0, expected.Length);
+            }
+            return str;
+        }
+        public static string remove_end(this string str, string expected)
+        {
+            if (str.EndsWith(expected))
+            {
+                return str.Remove(str.Length - expected.Length, expected.Length);
+            }
+            return str;
+        }
+        public static string brackets(this string str, string expected_start, string expected_end)
+        {
+            return str.remove_start(expected_start).remove_end(expected_end);
+        }
+        public static string brackets(this string str, string expected)
+        {
+            return str.remove_start(expected).remove_end(expected);
+        }
+
+    }
+}
