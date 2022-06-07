4f0a2cc9bacc33c228a202577680fa3202c2f509
Crutogurch <92383654+Crutogurch@users.noreply.github.com>
2022-06-07 21:22:44 +0500

    Update std.cpp
    INT_MAX, INT_MIN, SHORT_MAX, SHORT_MIN, LONG_MAX, LONG_MIN, BYTE_MAX, BYTE_MIN, UINT_MAX, USHORT_MAX, ULONG_MAX

--- Compiler/ApatiteLibraries/std.cpp
+++ Compiler/ApatiteLibraries/std.cpp
@@ -1,3 +1,17 @@
+#define INT_MAX 2147483647
+#define INT_MIN -2147483648
+#define SHORT_MAX 32767
+#define SHORT_MIN -32768
+#define LONG_MAX 2147483647
+#define LONG_MIN -2147483648
+#define BYTE_MAX 127
+#define BYTE_MIN -128
+
+#define UINT_MAX 4294967295
+#define USHORT_MAX 65535
+#define ULONG_MAX 4294967295
+
+
 
 void __FUNCTION__APATITE__print(const char* s)
 {
